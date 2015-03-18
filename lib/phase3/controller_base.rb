a1010b4c069b221ddf83feace336f25eb5c8d86b
Elliot Reed <elliotreed2@gmail.com>
2015-03-17 18:14:10 -0700

    phase VIb

--- lib/phase3/controller_base.rb
+++ lib/phase3/controller_base.rb
@@ -2,12 +2,17 @@ require_relative '../phase2/controller_base'
 require 'active_support'
 require 'active_support/core_ext'
 require 'erb'
+require 'active_support/inflector'
 
 module Phase3
   class ControllerBase < Phase2::ControllerBase
     # use ERB and binding to evaluate templates
     # pass the rendered html to render_content
     def render(template_name)
+      filename = File.expand_path("~/Desktop/skeleton") + "/views/#{self.class.name.underscore}/#{template_name}.html.erb"
+      puts "filename is #{filename}"
+      content = ERB.new(File.read(filename)).result(binding)
+      render_content(content, "text/html")
     end
   end
 end
--- lib/phase5/params.rb
+++ lib/phase5/params.rb
@@ -10,9 +10,18 @@ module Phase5
     # You haven't done routing yet; but assume route params will be
     # passed in as a hash to `Params.new` as below:
     def initialize(req, route_params = {})
+      # decoded_string = if req.query_string
+      #   parse_www_encoded_form(req.query_string)
+      # else
+      #   {}
+      # end
+      @params = decode_string(req.query_string)
+                .merge(decode_string(req.body))
+                .merge(route_params)
     end
 
     def [](key)
+      @params[key.to_s]
     end
 
     def to_s
@@ -27,12 +36,62 @@ module Phase5
     # user[address][street]=main&user[address][zip]=89436
     # should return
     # { "user" => { "address" => { "street" => "main", "zip" => "89436" } } }
-    def parse_www_encoded_form(www_encoded_form)
+    def parse_nested_keys(keys, value)
+      if keys.empty?
+        value
+      else
+        { keys[0] => parse_nested_keys(keys[1..-1], value) }
+      end
     end
+
+    def parse_www_encoded_form(www_encoded_form)
+      debugger
+      key_values = www_encoded_form.split('&')
+      parsed_hash = {}
+      key_values.each do |key_value|
+        key_value = key_value.split('=')
+        keys = key_value[0]
+        value = key_value[-1]
+        keys = parse_key(keys)
+        new_value = parse_nested_keys(keys[1..-1], value)
+        if parsed_hash[keys[0]].is_a?(Hash)
+          parsed_hash[keys[0]] = friendly_merge(parsed_hash[keys[0]], new_value)
+        else
+          parsed_hash[keys[0]] = new_value
+        end
+      end
+      parsed_hash
+    end
+
+    def friendly_merge(hash1, hash2)
+      new_hash = {}
+      hash1.each do |key, value|
+        if value.is_a?(Hash) && hash2[key].is_a?(Hash)
+          new_hash[key] = friendly_merge(value, hash2[key])
+        else
+          new_hash[key] = value
+        end
+      end
+      hash2.merge(new_hash)
+    end
+
+
+    #   end
+    #   URI.decode_www_form(www_encoded_form).to_h
+    # end
 
     # this should return an array
     # user[address][street] should return ['user', 'address', 'street']
     def parse_key(key)
+      key.split(/\]\[|\[|\]/)
+    end
+
+    def decode_string(query_string)
+      if query_string
+        parse_www_encoded_form(query_string)
+      else
+        {}
+      end
     end
   end
 end
